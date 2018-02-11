00b27d5bb29d15e85e2c065b29823c928f16c485
Unknown <cokurows14@gmail.com>
2018-02-11 16:05:31 -0600

    Continued to work on the Lights class

--- pi-rgb-ui/src/Components/Lights.js
+++ pi-rgb-ui/src/Components/Lights.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react"
-import WebSocket from "../Utils/WebSocket";
+import { ChromePicker } from "react-color"
+
+import LightStore from "../Stores/LightsStore"
+
+// TODO: temp
+import WebSocket from "../Utils/WebSocket"
+
 
 /**
  * @namespace Components
  */
+
+
+/**
+ * @author Cody Kurowski
+ * @enum {number}
+ * @memberOf Components.Lights
+ */
+const LEDEnum = {
+    /** Radius of LED circle */
+    RADIUS: 20,
+    /** X Distance from center of one LED circle to the next */
+    DISTANCEBETWEENXPOS: 50,
+    /** X Distance from center of one LED circle to the next */
+    DISTANCEBETWEENYPOS: 50,
+    /** X Positsion of the first LED circle */
+    XPOSSTART: 30,
+    /** Y Positsion of the first LED circle */
+    YPOSSTART: 30,
+}
 
 
 /**
@@ -17,14 +42,79 @@ import WebSocket from "../Utils/WebSocket";
 class Lights extends Component {
     constructor() {
         super()
-        this.state = {
 
+        this.state = {
+            lightsArr: LightStore.getLights(),
+            width: 0,
+            height: 0,
         }
+
+        this.getCanvasHeight = this.getCanvasHeight.bind(this)
+        this.handleColorChange = this.handleColorChange.bind(this)
+        this.handleLightsChanged = this.handleLightsChanged.bind(this)
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+        this.updateCanvas = this.updateCanvas.bind(this)
+        this.makeLED = this.makeLED.bind(this)
+    }
+
+    componentWillMount() {
+        window.addEventListener("resize", this.updateWindowDimensions)
+        LightStore.on("LIGHTS_CHANGED", this.handleLightsChanged)
     }
 
     componentDidMount() {
+        // TODO: Move this tho somewhere else
         WebSocket.openWebSocket()
+
+        this.updateWindowDimensions()
         this.updateCanvas()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWindowDimensions)
+        LightStore.removeListener("LIGHTS_CHANGED", this.handleLightsChanged)
+    }
+
+    /**
+     * @author Cody Kurowski
+     * @description This function calculates the proper height of the canvas.
+     * 
+     * @returns {number} Height of canvas
+     * @memberof Components.Lights
+     */
+    getCanvasHeight() {
+        const numberOfLEDsPerRow = Math.floor(this.state.width / LEDEnum.DISTANCEBETWEENXPOS)
+        const numberOfRows = Math.ceil(this.state.lightsArr.length / numberOfLEDsPerRow)
+        const height = ((numberOfRows - 1) * LEDEnum.DISTANCEBETWEENYPOS) + (LEDEnum.YPOSSTART * 2)
+        return height
+    }
+
+    handleColorChange(color) {
+        console.log(color.rgb)
+    }
+
+    handleLightsChanged() {
+        // TODO: Stuff
+    }
+
+    /**
+     * @author Cody Kurowski
+     * @description This function is called when the browser window is resized.
+     * This function will reset the width and height state to the current demensions.
+     * When called this will re-draw the canvas.
+     * 
+     * @memberof Components.Lights
+     */
+    updateWindowDimensions() {
+        this.setState({ 
+            width: window.innerWidth, 
+        }, () => {
+            this.setState({
+                height: this.getCanvasHeight(),
+            }, () => {
+                this.updateCanvas()
+            })
+        })
     }
 
     /**
@@ -36,14 +126,60 @@ class Lights extends Component {
      */
     updateCanvas() {
         const ctx = this.canvas.getContext("2d")
+
+        // Init starting positions
+        let xPos = LEDEnum.XPOSSTART
+        let yPos = LEDEnum.YPOSSTART
+
+        // Make multiple LEDs
+        this.state.lightsArr.forEach((LED) => {
+            // Make color
+            const color = `rgba(${LED.R}, ${LED.G}, ${LED.B}, 1)`
+
+            this.makeLED(ctx, xPos, yPos, LEDEnum.RADIUS, color)
+
+            // Check to see if the next one will be rendered outside the canvas
+            const checkXPos = xPos + LEDEnum.DISTANCEBETWEENXPOS + (LEDEnum.XPOSSTART)
+            if (checkXPos >= this.state.width) {
+                // Go down to the next line and set XPos back to start
+                yPos += LEDEnum.DISTANCEBETWEENYPOS 
+                xPos = LEDEnum.XPOSSTART
+            } else {
+                xPos += LEDEnum.DISTANCEBETWEENXPOS
+            }  
+        })   
+    }
+
+    /**
+     * @author Cody Kurowski
+     * @description This function draws each individual LED light 
+     * 
+     * @param {object} ctx Canvas Object
+     * @param {number} XPos X position of center of the circle
+     * @param {number} YPos Y position of center of the circle
+     * @param {number} radius Radius of the circle
+     * @param {string} color Color of the circle
+     * @memberof Components.Lights
+     */
+    makeLED(ctx, XPos, YPos, radius, color) {
         ctx.beginPath()
-        ctx.arc(95, 50, 40, 0, 2 * Math.PI)
+        ctx.shadowBlur = 10
+        ctx.shadowColor = color
+        ctx.arc(XPos, YPos, radius, 0, 2 * Math.PI)
+        ctx.fillStyle = color
+        ctx.fill()
+        ctx.strokeStyle = color
         ctx.stroke()
     }
 
     render() {
+        const { width, height } = this.state
+
         return (
-            <canvas ref={(canvas) => { this.canvas = canvas }} width={600} height={600} />
+            <div className="text-center">
+                <canvas ref={(canvas) => { this.canvas = canvas }} width={width} height={height} />
+                <ChromePicker disableAlpha={true} onChangeComplete={this.handleColorChange} />
+            </div>
         )
     }
 }
---
+++ pi-rgb-ui/src/Components/MockLights.js
@@ -0,0 +1,69 @@
+// TODO: Delete this file when done using it? 
+
+const LEDsObject = {
+    Type: "",
+    Data: [
+        {
+            LEDPosition: 1,
+            R: 255,
+            G: 255,
+            B: 255,
+        },
+        {
+            LEDPosition: 2,
+            R: 255,
+            G: 255,
+            B: 255,
+        },
+        {
+            LEDPosition: 3,
+            R: 255,
+            G: 255,
+            B: 255,
+        },
+        {
+            LEDPosition: 4,
+            R: 255,
+            G: 255,
+            B: 255,
+        },
+        {
+            LEDPosition: 5,
+            R: 255,
+            G: 255,
+            B: 255,
+        },
+        {
+            LEDPosition: 6,
+            R: 255,
+            G: 255,
+            B: 255,
+        }, 
+        {
+            LEDPosition: 7,
+            R: 255,
+            G: 255,
+            B: 255,
+        }, 
+        {
+            LEDPosition: 8,
+            R: 255,
+            G: 255,
+            B: 255,
+        }, 
+        {
+            LEDPosition: 9,
+            R: 255,
+            G: 0,
+            B: 0,
+        }, 
+        {
+            LEDPosition: 10,
+            R: 255,
+            G: 0,
+            B: 0,
+        },
+    ],
+}
+
+export default LEDsObject
